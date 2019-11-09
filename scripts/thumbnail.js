715475cbfd089b1867ee302500e6e7003711b5b3
huangjing <sn-huangjing@pptv.com>
2019-11-09 11:46:21 +0800

    observablehq plugin

--- scripts/thumbnail.js
+++ scripts/thumbnail.js
@@ -1,9 +1,26 @@
+let hashCode = s => Math.abs(s.split('').reduce((a,b)=>{a=((a<<5)-a)+b.charCodeAt(0);return a&a},0))
+
+
 /**
-* Thumbnail Helper
-* @description Get the thumbnail url from a post
-* @example
-*     <%- thumbnail(post) %>
-*/
-hexo.extend.helper.register('thumbnail', function (post) {
-    return post.thumbnail || post.banner || '';
+ * Thumbnail Helper
+ * @description Get the thumbnail url from a post
+ * @example
+ *     <%- thumbnail(post) %>
+ */
+hexo.extend.helper.register("thumbnail", function(post) {
+  return post.thumbnail || post.banner || "";
 });
+
+hexo.extend.tag.register("obs", function(args) {
+  let id = args[0];
+  let hash = hashCode(id);
+  return `
+            <div id="note_${hash}"></div>
+            <script type="module">
+                import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
+                import define from "${id}";
+                const runtime = new Runtime();
+                const main = runtime.module(define, Inspector.into(document.querySelector('#note_${hash}')));
+            </script>`;
+});
+
